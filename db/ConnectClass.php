b8a187722db17e49436b3e3284b08612d0b20855
Ariel <arielmello@rede.ulbra.br>
2020-11-10 07:27:39 -0300

    DB

---
+++ db/ConnectClass.php
@@ -0,0 +1,67 @@
+<?php
+
+class connectClass{
+
+    private $conn;
+
+    private $host;
+    private $userDB;
+    private $passDB;
+    private $nameDB;
+
+    public function openConn(){
+
+        $this -> conn = mysqli_connect($this -> getHost(), $this -> getUser(),
+        $this -> getPassword(), $this -> getNameDB());
+
+        mysqli_set_charset($this -> conn, 'utf8');
+
+        if(mysqli_errno($this -> conn)){
+            echo 'Banco de dados não conectado, erro: ' . mysqli_error($this -> conn);
+        } else {
+            echo '<h1>Tudo certo!</h1>';
+        }
+    }
+
+    public function getConn(){
+        return $this -> conn;
+    }
+
+    //Getters e setters
+
+    public function __construct($host, $user, $pass, $name){
+        $this -> setHost($host);
+        $this -> setUser($user);
+        $this -> setPassword($pass);
+        $this -> setNameDB($name);
+    }
+    
+
+    private function setHost($host){
+        $this -> host = $host;
+    }
+    private function getHost(){
+        return $this -> host;
+    }
+
+    private function setUser($userDB){
+        $this -> userDB = $userDB;
+    }
+    private function getUser(){
+        return $this -> userDB;
+    }
+
+    private function setPassword($passDB){
+        $this -> passDB = $passDB;
+    }
+    private function getPassword(){
+        return $this -> passDB;
+    }
+
+    private function setNameDB($nameDB){
+        $this -> nameDB = $nameDB;
+    }
+    private function getNameDB(){
+        return $this -> nameDB;
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once('db/ConnectClass.php');
+$sis_mercado = new connectClass('localhost:3306', 'root', '', 'sis_mercado');
+$sis_mercado -> openConn();
+
+?>
