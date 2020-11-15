492788b02deac0f7eb9bc4c2bbbad7df9f597f73
Ariel <arielmello@rede.ulbra.br>
2020-11-15 02:11:13 -0300

    table styles

--- views/pages/home.php
+++ views/pages/home.php
@@ -5,35 +5,27 @@
 
     function listUsers(){
     
-    require_once('models/ProductsModel.php');
+        require_once('models/ProductsModel.php');
 
-    $Products_model = new productsModel();
-    $result = $Products_model->listProducts();
+        $Products_model = new productsModel();
+        $result = $Products_model->listProducts();
 
-    echo '<table class="table">';
+        echo '<table class="table">';
 
-    echo '<thead><tr><th scope="col">ID: </th>' . '<th scope="col">Nome: </th>' . '<th scope="col">E-mail: </th>' . '<th scope="col">Criado em: </th>' . '<th scope="col">Modificado em: </th></thead>';
+        echo '<thead><tr><th scope="col">ID: </th>' . '<th scope="col">Nome: </th>' . '<th scope="col">E-mail: </th>' . '<th scope="col">Criado em: </th>' . '<th scope="col">Modificado em: </th></thead>';
 
-    while($row_user = mysqli_fetch_assoc($result)){
-    
-        echo '<tbody><tr>' . $row_user['id'] . '</tr>';
-
-        echo '<tr>' . $row_user['nome'] . '</tr>';
-
-        echo '<tr>' . $row_user['email'] . '</tr>';
-        
-        echo '<tr>' . $row_user['created'] . '</tr></tbody>';
-        
-        echo '<tr>'; 
-            if($row_user['modified'] === NULL){
-                echo 'Não à modificações';
-            } 
-        echo '</tr></table>';
+        while($row_user = mysqli_fetch_assoc($result)){
+            echo '<tr><th>' . $row_user['id'] . '</th>';
+            echo '<td>' . $row_user['nome'] . '</td>';
+            echo '<td>' . $row_user['email'] . '</td>';
+            echo '<td>' . $row_user['created'] . '</td>';
+            if($row_user['modified'] === null){
+                echo '<td>Não existem modificações</td></tr>';
+            } else {
+                echo '<td>' . $row_user['modified'] . '</td></tr>';
+            }
+        }
 
     }
-
-    }
-
-    
 
 ?>
