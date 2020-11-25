615ec12d6a1ee6b44afdb9cc4ca99615925f8564
Ariel <arielmello@rede.ulbra.br>
2020-11-24 21:36:45 -0300

    clientsModel

---
+++ models/ClientsModel.php
@@ -0,0 +1,21 @@
+<?php
+
+class clientsModel {
+
+    public $result;
+    
+    public function listProducts(){
+        require_once('db/ConnectClass.php');
+        $Oconn = new connectClass('localhost:3306', 'root', '', 'sis_mercado');
+        $Oconn->openConn();
+        $conn = $Oconn->openConn();
+
+        $sql = 'SELECT * FROM `usuarios`';
+
+        $this -> result = mysqli_query($conn, $sql);
+
+        return $this -> result;
+
+    }
+
+}
